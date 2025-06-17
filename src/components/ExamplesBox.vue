f22cf9d76dd88ea449ff112e0a28faea43210f10
yyhhenry <yyhhenry@foxmail.com>
2025-06-17 23:41:35 +0800

    style: improve layout of ExamplesBox component
    Updated the ExamplesBox component to enhance its layout by ensuring proper overflow handling and alignment of elements.

--- src/components/ExamplesBox.vue
+++ src/components/ExamplesBox.vue
@@ -5,10 +5,13 @@ defineEmits<{
 const regExamples = ['(a|b)*aab', '1(1010*|1(010)*1)*0', 'a((a|b)*|ab*a)*b'];
 </script>
 <template>
-  <div class="d-flex align-center ga-3 pa-2">
-    <div class="text-h5 mx-2">Examples:</div>
+  <div
+    class="d-flex align-center ga-3 pa-2"
+    style="overflow-x: auto; white-space: nowrap"
+  >
+    <div class="text-h5 mx-2 flex-shrink-0">Examples:</div>
     <v-btn
-      class="text-body-1"
+      class="text-body-1 flex-shrink-0"
       variant="outlined"
       v-for="example in regExamples"
       :key="example"
